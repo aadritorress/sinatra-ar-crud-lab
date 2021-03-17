8370dbd12048d5ae3e8c8536de1b145779128729
Adriana <adriana.amaral@msn.com>
2021-03-17 12:41:08 -0500

    commit tree 476dcfcbfadfca001a5eb83248b4c716b77fd7b2

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,17 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+
+    get '/articles' do 
+        @articles.all 
+
+        erb :index
+    end 
+
+
+    get '/articles/:id' do 
+   @article = params[:id]
+        erb :show 
+    end 
+
 end
